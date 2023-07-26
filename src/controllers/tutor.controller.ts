3148a09db683a62058c52354ef2e3b1d323dde49
doni-zete <donizete112@gmail.com>
2023-07-26 12:27:10 -0300

    controller to create a tutor

--- src/controllers/tutor.controller.ts
+++ src/controllers/tutor.controller.ts
@@ -3,9 +3,26 @@ import { Request, Response } from "express";
 
 const findAllTutorsController = async (req: Request, res: Response) => {
   try {
-    res.send(await tutorService.findAllTutorsService);
-  } catch (error) {}
+    res.send(await tutorService.findAllTutorsService());
+  } catch (error) {
+    res.status(500).send({ message: String });
+    console.log(`Erro inesperado, tente novamente ${error}`);
+  }
 };
+
+const createTutorController = async (req: Request, res: Response) => {
+  try {
+    const corpo = {
+      ...req.body,
+    };
+    res.send(await tutorService.createTutorService(corpo));
+  } catch (error) {
+    res.status(500).send({ message: String });
+    console.log(`Erro inesperado, tente novamente ${error}`);
+  }
+};
+
 export default {
   findAllTutorsController,
+  createTutorController,
 };
