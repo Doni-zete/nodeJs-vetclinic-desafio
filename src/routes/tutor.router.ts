4fddc7ca4285881ab9a2d3c65d73a9a16b26db05
doni-zete <donizete112@gmail.com>
2023-07-27 14:08:38 -0300

    feat: adding  pet route

--- src/routes/tutor.router.ts
+++ src/routes/tutor.router.ts
@@ -7,9 +7,12 @@ const router = Router();
 router.get("/findAll", tutorController.findAllTutorsController);
 
 router.post("/create", tutorController.createTutorController);
+router.post("/createPet/:id",tutorController.createPetTutorController)
 
 router.put("/update/:id", tutorController.updateTutorController);
 
 router.delete("/remove/:id", tutorController.deleteTutorController);
+
+
 
 export default router;
