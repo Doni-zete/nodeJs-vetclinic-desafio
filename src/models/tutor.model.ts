e05e23f45b639f25ef7b482da196d27f667e32f6
doni-zete <donizete112@gmail.com>
2023-07-27 11:42:56 -0300

    feat: adding pet scheme

--- src/models/tutor.model.ts
+++ src/models/tutor.model.ts
@@ -4,8 +4,17 @@ const TutorSchema = new Schema({
   name: { type: String, required: true },
   phone: { type: String, require: true },
   email: { type: String, required: true },
-  date_of_birth: { type: Date },
-  zip_code: {type: String, required: true}
+  date_of_birth: { type: String },
+  zip_code: { type: String, required: true },
+  pets: [
+    {
+      name: { type: String, required: true },
+      species: { type: String, required: true },
+      carry: { type: String, required: true },
+      weight: { type: Number, required: true },
+      date_of_birth: { type: String, required: true },
+    },
+  ],
 });
 
 const Tutor = model("Tutors", TutorSchema);
