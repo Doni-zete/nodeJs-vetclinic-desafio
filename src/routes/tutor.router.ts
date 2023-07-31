dc299eee4257ef16f2f33df55b1b20d81b27d689
doni-zete <donizete112@gmail.com>
2023-07-30 21:51:02 -0300

    fix: add validationTutor

--- src/routes/tutor.router.ts
+++ src/routes/tutor.router.ts
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get("/tutors", tutorController.findAllTutorsController);
 router.post("/tutor", validationTutor,tutorController.createTutorController);
-router.put("/tutor/:id", tutorController.updateTutorController);
+router.put("/tutor/:id",validationTutor, tutorController.updateTutorController);
 router.delete("/tutor/:id", tutorController.deleteTutorController);
 
 router.post('/pet/:tutorId',validationPet,tutorController.createPetTutorController);
