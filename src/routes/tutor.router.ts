164a22d3a96ba1c7c7236d64ded7baff4664e7ad
doni-zete <donizete112@gmail.com>
2023-07-30 21:34:31 -0300

    feat:adding validation adding pet and tutor route validation

--- src/routes/tutor.router.ts
+++ src/routes/tutor.router.ts
@@ -1,19 +1,18 @@
 import { Router } from "express";
- import tutorController from "../controllers/tutor.controller"
+import * as tutorController from "../controllers/tutor.controller"
+import {validationTutor,validationPet} from "../middleware/validation"
 
 const router = Router();
 
 
 router.get("/tutors", tutorController.findAllTutorsController);
-router.post("/tutor", tutorController.createTutorController);
+router.post("/tutor", validationTutor,tutorController.createTutorController);
 router.put("/tutor/:id", tutorController.updateTutorController);
 router.delete("/tutor/:id", tutorController.deleteTutorController);
 
-router.post('/pet/:tutorId', tutorController.createPetTutorController);
-router.put('/pet/:petId/tutor/:tutorId', tutorController.updatePetTutorController);
+router.post('/pet/:tutorId',validationPet,tutorController.createPetTutorController);
+router.put('/pet/:petId/tutor/:tutorId', validationPet,tutorController.updatePetTutorController);
 router.delete('/pet/:petId/tutor/:tutorId', tutorController.deletePetTutorController);
-
-
 
 
 export default router;
